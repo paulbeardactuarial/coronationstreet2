36cda49b2c2a42a3d26c6425d0409e29a46bc2ca
Paul Beard <paulbread88@gmail.com>
2025-05-19 11:40:21 +0100

    more updates to date cleansing, includign  multi date

--- cleanse_data.py
+++ cleanse_data.py
@@ -25,7 +25,9 @@ def triage_date_string_fmt(
         string_patterns={
             "full": r'^\s*\d{1,2}(st|nd|rd|th)\s+(January|February|March|April|May|June|July|August|September|October|November|December)\s+\d{4}\s*$',
             "month_year": r'(January|February|March|April|May|June|July|August|September|October|November|December)\s+\d{4}\s*$',
-            "year": r'(c.\s*|)\d{4}$'
+            "year": r'(c.\s*|)\d{4}$',
+            "multi": r'(\s*\d{1,2}(st|nd|rd|th)\s+(January|February|March|April|May|June|July|August|September|October|November|December)\s+\d{4}\s*)',
+            "other": r'.'
         }
 ):
 
@@ -39,7 +41,7 @@ def triage_date_string_fmt(
         np.select(
             condlist=conditions,
             choicelist=format_names,
-            default="other"
+            default=pd.NA
         ),
         index=date_string_series.index
     )
@@ -75,7 +77,7 @@ def cleanse_month_year(date_series, seed=42):
 
 def cleanse_year(date_series, seed=42):
 
-    date_series = date_series.str.re
+    date_series = date_series.str.replace("c.", "").str.strip()
     date_series = date_series.apply(
         lambda x: datetime.datetime.strptime(x, "%Y")
     )
@@ -102,11 +104,6 @@ def cleanse_year(date_series, seed=42):
 
 
 # %%
-date_string_series = c_life_death["Born"]
-date_string_formats = triage_date_string_fmt(date_string_series)
-
-
-# %%
 # functions that handle cleansing process
 
 def cleanse_full(date_string_series):
@@ -117,11 +114,73 @@ def cleanse_full(date_string_series):
     return date_strings
 
 
+def cleanse_full_incl_multi(
+        date_string_series,
+        full_date_pattern=r'(\s*\d{1,2}(st|nd|rd|th)\s+(January|February|March|April|May|June|July|August|September|October|November|December)\s+\d{4}\s*)'
+):
+    multi_full_extract = date_string_series.str.extractall(full_date_pattern)[
+        0]
+    first_full_extract = multi_full_extract.xs(key=0, level=1).str.strip()
+    cleansed_date_string_series = first_full_extract.combine_first(
+        date_string_series)
+    date_strings = cleanse_full(cleansed_date_string_series)
+
+    return date_strings
+
+
+# %%
+
+
+all_dates = c_life_death.melt(
+    id_vars="Character",
+    value_vars=["Born", "Died", "First appearance", "Last appearance"],
+    value_name="Value"
+)
+
+
+date_string_series = all_dates["Value"].str.strip()
+date_string_formats = triage_date_string_fmt(date_string_series)
+
+# get the strings for each type
 full_date_strings = date_string_series[date_string_formats == "full"]
 month_year_date_strings = date_string_series[date_string_formats == "month_year"]
 year_strings = date_string_series[date_string_formats == "year"]
+multi_strings = date_string_series[date_string_formats == "multi"]
 
-month_year_date_strings
-cleanse_full(full_date_strings)
-cleanse_month_year(month_year_date_strings)
-cleanse_year(year_strings)
+# get the cleansed strings for each type
+cleansed_dates = {
+    "full": cleanse_full(full_date_strings),
+    "month_year": cleanse_month_year(month_year_date_strings),
+    "year": cleanse_year(year_strings),
+    "multi": cleanse_full_incl_multi(multi_strings)
+}
+
+# get an NA array for anything not triggered
+na_date_string_series = date_string_series.values
+na_date_string_series[:] = pd.NA
+
+# stitch it all back together
+cleansed_dates_combined = pd.Series()
+for key in cleansed_dates.keys():
+    cleansed_dates_combined = cleansed_dates_combined.combine_first(
+        cleansed_dates[key])
+
+cleansed_dates_combined
+# all_dates.Value
+len(na_date_string_series)
+
+
+# %%
+
+all_dates = c_life_death.melt(
+    id_vars="Character",
+    value_vars=["Born", "Died", "First appearance", "Last appearance"],
+    value_name="Value"
+)
+
+leftover_dates = all_dates[(triage_date_string_fmt(
+    all_dates.Value) == "other") & (all_dates.Value.notna())]
+
+leftover_dates.Value.str.replace('\s\((1|2|3|4)\)', '', regex=True).unique()
+
+# %%
