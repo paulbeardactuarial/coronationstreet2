a16ecdfeb8539d4fd6c17afa3c38608156f86dab
Paul Beard <paulbread88@gmail.com>
2025-06-27 19:15:21 +0100

    experience start

--- construct_experience_table.py
+++ construct_experience_table.py
@@ -1,7 +1,30 @@
 # %%
 import pandas as pd
 import numpy as np
-
+import datetime
 
 # %%
 corrie_segments = pd.read_csv("./Data/character_data_segmented.csv")
+corrie_segments = corrie_segments[~corrie_segments["Born"].isna(
+)].reset_index().drop("index", axis=1)
+
+# convert to datetime format
+date_fields = ["Born", "Died", "First appearance",
+               "Last appearance", "exit_date", "start_date"]
+corrie_segments.loc[:, date_fields] = corrie_segments.loc[:,
+                                                          date_fields].apply(pd.to_datetime)
+
+
+# %%
+
+# drop those without "Born" day
+
+# corrie_segments["exit_date"].apply(lambda x: datetime.datetime.strptime(x, format = 'd-m-Y'))
+
+# applying datetime.datetime.strptime()
+# pd.dt.to_timestamp()
+# pd.dt.to_period()
+
+
+# .dt.to_timestamp()
+# corrie_segments["Born"].year
