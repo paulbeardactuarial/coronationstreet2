618153c7dd5163b31316a6608ece41ff07981a48
Paul Beard <paulbread88@gmail.com>
2025-07-07 18:53:11 +0100

    developing experience analysis function

--- construct_experience_table.py
+++ construct_experience_table.py
@@ -4,22 +4,38 @@ import numpy as np
 import datetime
 
 # %%
-corrie_segments = pd.read_csv("./Data/character_data_segmented.csv")
-corrie_segments = corrie_segments[~corrie_segments["Born"].isna(
-)].reset_index().drop("index", axis=1)
-
-# convert to datetime format
-date_fields = ["Born", "Died", "First appearance",
-               "Last appearance", "Exit date", "Start date"]
-corrie_segments.loc[:, date_fields] = corrie_segments.loc[:,
-                                                          date_fields].apply(pd.to_datetime)
-
+input_fp = "./Data/character_data_segmented.parquet"
+df = pd.read_parquet(input_fp)
 
 # %%
 
 # drop those without "Born" day
+df = df[~df["Born"].isna()]
 
-# corrie_segments["exit_date"].apply(lambda x: datetime.datetime.strptime(x, format = 'd-m-Y'))
+
+# %%
+
+# def carve_experience_single_year(df):
+exp_year = 2000
+
+
+period_start = pd.Timestamp(datetime.date(exp_year, 1, 1))
+period_end = pd.Timestamp(datetime.date(exp_year, 12, 31))
+
+experience_start_field = "StartDate"
+experience_end_field = "ExitDate"
+
+# filter for rows that have experience within period
+in_force_df = df[
+    (df[experience_start_field] <= period_end) & (
+        df[experience_end_field] >= period_start)
+].copy()
+
+for field in [experience_start_field, experience_end_field]:
+    in_force_df[field] = in_force_df[field].clip(
+        lower=period_start,
+        upper=period_end
+    )
 
 # applying datetime.datetime.strptime()
 # pd.dt.to_timestamp()
@@ -28,3 +44,5 @@ corrie_segments.loc[:, date_fields] = corrie_segments.loc[:,
 
 # .dt.to_timestamp()
 # corrie_segments["Born"].year
+
+# %%
